76706eb215e18e868976ec0123e96eb115ad5900
Tom Dhinsa <mrdhinsa05@gmail.com>
2022-01-28 22:14:36 -0500

    Adds MovieGenre association table

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,7 @@ class Movie < ApplicationRecord
   validates :year, presence: true, numericality: { greater_than: 1994 }
   validates :plot, presence: true, length: { in: 20..700 }
   validates :director, presence: true
-
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   has_many :actors
 end
